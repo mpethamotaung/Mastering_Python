78a54eebc2850d21eb1aefa9ea517b9c9e205b07
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-08 17:52:31 +0200

    treasure island and name update

---
+++ 100Days_with_Angela/treasure_island.py
@@ -0,0 +1,50 @@
+"""Treasure Island game. This game is designed to test understanding of control flow
+statements ( if, else, elif)"""
+
+#Flow chart download:
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+choice1 = input("You're accross the road. Where do you want to go? left or right?\n").lower()
+
+if choice1 == "left":
+    choice2 = input("Do you want to swim or wait?\n").lower()
+    if choice2 == "wait":
+        choice3 = input("Which door?\n").lower()
+        if choice3 == "red":
+            print("Fire! Game over")
+        elif choice3 == "yellow":
+            print("You've found the treasure")
+        elif choice3 == "blue":
+            print("You fell into the sea. Game over")
+        else: 
+            print("You didn't choose any door. Game over")
+    else:
+        print("Game Over")
+
+else:
+    print(" Game over")
