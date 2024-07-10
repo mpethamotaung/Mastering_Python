2778b8d96f293992270662252f0fb09a55c105ea
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-10 22:13:27 +0200

    list data structure basics

---
+++ 100Days_with_Angela/list_datastructure.py
@@ -0,0 +1,18 @@
+states_of_sa = ["Gauteng", "Free State", "Cape Town", "Kwazulu-Natal"]
+
+
+#Add another state to the list
+states_of_sa.append("North-West")
+#extend the list of states
+states_of_sa.extend(["western Cape", "Eastern Cape"])
+
+print(states_of_sa)
+
+#states_of_usa = ["Delaware", "New Jersey", "Georgia"]
+
+#zipped = zip(states_of_sa,states_of_usa)
+
+#print(tuple(zipped))
+
+#for(x,y) in zipped:
+    #print(x,y)
