3b04a189f8825626c6263f391a24aa2b5a6367f3
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-10 21:21:00 +0200

    random rule and random basics (random integer, random float, random score).

---
+++ 100Days_with_Angela/random_rule.py
@@ -0,0 +1,11 @@
+import random
+
+#Random Integer
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#Random float
+random_float = random.random() * 5 #Random float with range 1 - 5
+
+love_score = random.randint(1,100)
+print(f"Your love score is: {love_score}")
