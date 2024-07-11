9c02f75845e4b3bf3b3adb65827e0bba8b7d2282
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-11 19:42:13 +0200

    initiate treasure_map.py

--- 100Days_with_Angela/dirty_dozen.py
+++ 100Days_with_Angela/dirty_dozen.py
@@ -1,10 +1,15 @@
 """IndexErrors and working with nested lists"""
 
-dirt_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches",
-              "Cherries", "Tomatoes", "Celery", "Potatoes"]
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 
-#Separate the list into fruit and vegetables(unrelated)
+#Nested list
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
+
+""" 
+zipped = zip(fruits,vegetables)
+print(tuple(zipped))
 """
-    fruits = ["Strawberries", "Nectarines", "Apples", Grapes", "Peaches", "Cherries", "Pears"]
-    vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
-"""
+
---
+++ 100Days_with_Angela/treasure_map.py
@@ -0,0 +1,12 @@
+#Using aqcuired list knowledge to build a treasure map
+
+line1 = ['','','']
+line2 = ['','','']
+line3 = ['','','']
+map = [line1, line2,line3]
+print("Hiding your treasure! X marks the spot")
+position = input() #Where do you want to put the treasure?
+
+
+
+print(f'{line1}\n{line2}\n{line3}')
