b07ae803b83f250c7cab79fb9cdea01bcd9034d1
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-12 15:56:04 +0200

    add letter variable to get the first letter of input : letter = position[0].lower()

--- 100Days_with_Angela/treasure_map.py
+++ 100Days_with_Angela/treasure_map.py
@@ -7,6 +7,7 @@ map = [line1, line2,line3]
 print("Hiding your treasure! X marks the spot")
 position = input() #Where do you want to put the treasure?
 
+letter = position[0].lower() #Grab first letter and make it lowercase
 
 
 print(f'{line1}\n{line2}\n{line3}')
