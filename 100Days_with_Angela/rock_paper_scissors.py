a15aa249361b90eff3dd5e9e9de792a1b4b31f04
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-12 18:44:11 +0200

    update and complete treasure map. init rock paper scissors game

---
+++ 100Days_with_Angela/rock_paper_scissors.py
@@ -0,0 +1,37 @@
+#Rock,Paper, Scissors game against the computer
+
+# Rock
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+# Paper
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+# Scissors
+scissors ="""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+import random
+
+user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: ")
+choice = ['Rock', 'Paper', 'Scissors']
+computer_choice = random.randint() 
+
--- 100Days_with_Angela/treasure_map.py
+++ 100Days_with_Angela/treasure_map.py
@@ -5,11 +5,17 @@ line2 = ['','','']
 line3 = ['','','']
 map = [line1, line2,line3]
 print("Hiding your treasure! X marks the spot")
-position = input("Add letter A,B,C followed by 1,2,3 (exampl: A1)") #Where do you want to put the treasure?
-
-letter = position[0].lower() #Grab first letter and make it lowercase
-abc = ['a','b','c'] #List that presents the three possible letters
-letter_index = abc.index(letter) #create a variable letter_index assign it to abc our list .index(letter)
+position = input("Add letter A,B,C followed by 1,2,3 (example: A1)") #Where do you want to put the treasure?
+#Grab first letter and make it lowercase
+letter = position[0].lower() 
+#List that presents the three possible letters
+abc = ['a','b','c'] 
+#create a variable letter_index assign it to abc our list .index(letter)
+letter_index = abc.index(letter) 
+#Take input that comes through input variable position
+number_index = int(position[1]) - 1 
+#Bring up nested list and include created indeces to insert 'x'
+map[number_index][letter_index] = "X"
 
 
 print(f'{line1}\n{line2}\n{line3}')
