8f02d5845dc26c2e7ac0e74280c5ef2ac3b2b74e
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-12 15:59:30 +0200

    create a list that represents the possible inputs: abc = ['a','b','c']

--- 100Days_with_Angela/treasure_map.py
+++ 100Days_with_Angela/treasure_map.py
@@ -5,9 +5,10 @@ line2 = ['','','']
 line3 = ['','','']
 map = [line1, line2,line3]
 print("Hiding your treasure! X marks the spot")
-position = input() #Where do you want to put the treasure?
+position = input("Add letter A,B,C followed by 1,2,3 (exampl: A1)") #Where do you want to put the treasure?
 
 letter = position[0].lower() #Grab first letter and make it lowercase
+abc = ['a','b','c'] #List that presents the three possible letters
 
 
 print(f'{line1}\n{line2}\n{line3}')
